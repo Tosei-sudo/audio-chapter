f13e0e0377174859bd0279d7775a536c2b2d9146
tosei.sudo <tosei.sudo@gmail.com>
2021-12-29 18:04:27 +0900

    1st Commit

--- src/app.vue
+++ src/app.vue
@@ -1,126 +1,345 @@
 <template>
-    <div v-cloak class="ln-wrapper">
-        <div class="ln-bg"></div>
-        <div class="ln-body">
-            <h2>LINE通知送信</h2>
-            <div class="ln-input">
-                <div>
-                    <span>
-                        送信先
-                        <select v-model="room">
-                            <option
-                                v-for="room in rooms"
-                                :key="room.id"
-                                :value="room.id"
-                            >
-                                {{ room.name }}
-                            </option>
-                        </select>
-                    </span>
-                </div>
-                <div>
-                    <label for="ln-alart-bool"
-                        ><input
-                            type="checkbox"
-                            id="ln-alart-bool"
-                            v-model="alart"
-                        />プッシュ通知
-                    </label>
-                </div>
-            </div>
-            <div class="ln-ctrl">
-                <input type="button" value="送信" class="ln-b ln-yes" />
-                <input type="button" value="キャンセル" class="ln-b ln-no" />
-            </div>
+    <div class="ap-wrapper">
+        <div class="ap-title">{{ currentTitle }}</div>
+        <div class="ap-ctrl" v-if="!loading">
+            <audio-controller
+                @play="c_play"
+                @pause="c_pause"
+                @next="next"
+                @back="back"
+                ref="controller"
+            ></audio-controller>
         </div>
+        <div v-if="!loading">
+            <audio-timebar
+                :max="max"
+                :marks="marks"
+                ref="audiotimebars"
+                @change="change"
+            ></audio-timebar>
+        </div>
+        <div class="ap-ts-editer">
+            <chapter-editer
+                :list="stamps"
+                @click="markclick"
+                :Auth="Auth"
+                ref="timestamp"
+                @chapteredit="chapteredit"
+                @del="del"
+            ></chapter-editer>
+        </div>
+        <div>
+            <chapter-modal
+                ref="modal"
+                :second="second"
+                :max="max"
+                :title="title"
+                @save="savechapter"
+                :cid="cid"
+                :Auth="Auth"
+                :userid="userid"
+                class="ap-ts-m"
+            ></chapter-modal>
+        </div>
+        <vue-loading
+            v-if="loading"
+            type="bars"
+            color="navy"
+            :size="{ width: '100px', height: '100px' }"
+        ></vue-loading>
     </div>
 </template>
 
 <script>
+import audiocontroller from "./components/music-controller.vue";
+import audiotimebar from "./components/music-timebar.vue";
+import chapterediter from "./components/chapter-editer.vue";
+import chaptermodal from "./components/chapter-modal.vue";
+import { VueLoading } from "vue-loading-template";
+import axios from "./axios";
 
+class api {
+    static EndPoint() {
+        return [
+            "/HumenFinder/php/audio/ChapterAPI",
+            "/HumenFinder/php/audio/AudioAPI",
+            "/HumenFinder/php/audio/GlobalChapterAPI",
+            "/HumenFinder/php/ctrl/AuthAPI",
+        ];
+    }
+
+    static get(AudioID) {
+        let d = axios.getRestParm({ AudioID });
+        return axios.get(this.EndPoint()[0], d);
+    }
+
+    static delete(ChapterID, UserID) {
+        let e = (UserID = 9999999 ? this.EndPoint()[2] : this.EndPoint()[0]);
+        let d = axios.getRestParm({ ChapterID });
+        return axios.delete(e, d);
+    }
+
+    static getMeta(AudioID) {
+        let d = axios.getRequestParm("getAudioList", { AudioID });
+        return axios.get(this.EndPoint()[1], d);
+    }
+
+    static post(AudioID, Title, Second, UserID = 9999999) {
+        let d = axios.getRestParm({
+            AudioID,
+            Title,
+            Second,
+        });
+        if (UserID != 9999999) {
+            return axios.post(this.EndPoint()[0], d);
+        } else {
+            return axios.post(this.EndPoint()[2], d);
+        }
+    }
+
+    static put(ChapterID, AudioID, Title, Second, UserID = 9999999 ,Auth = 0) {
+        let d = axios.getRestParm({
+            ChapterID,
+            AudioID,
+            Title,
+            Second,
+            UserID,
+        });
+        if (Auth == 0) {
+            return axios.put(this.EndPoint()[0], d);
+        } else {
+            return axios.put(this.EndPoint()[2], d);
+        }
+    }
+
+    static check() {
+        let d = axios.getRestParm({});
+        return axios.get(this.EndPoint()[3], d);
+    }
+}
 
 export default {
-    props: {
-        rooms: {
-            type: Array,
-            required: true,
-        },
-        message: {
-            type: String,
-            required: false,
-            default: "",
-        },
+    components: {
+        "audio-controller": audiocontroller,
+        "audio-timebar": audiotimebar,
+        VueLoading,
+        "chapter-editer": chapterediter,
+        "chapter-modal": chaptermodal,
     },
     data() {
         return {
-            room: "",
-            alart: true,
+            audio: null,
+            currentAudioID: 0,
+            currentpath: "",
+            currentTitle: "Loading...",
+            max: 0,
+            stamps: [],
+            loading: true,
+            second: 0,
+            title: "",
+            cid: NaN,
+            userid: NaN,
+            Auth: 0,
         };
     },
-    watch: {
-        rooms: function (newVal, oldVal) {
-            this.room = newVal[0].id;
+    created() {
+        let i = axios.getParam("a");
+        let a = [api.getMeta(i), api.get(i), api.check()];
+        Promise.all(a).then((res) => {
+            let m = res[0].data.Body.Data[0];
+            this.currentAudioID = m.AudioID;
+            this.currentTitle = m.Title;
+            this.currentpath = m.url;
+
+            res[1].data.Body.Data.forEach((item) => {
+                this.stamps.push([
+                    item.Second,
+                    item.Title,
+                    item.UserID,
+                    item.ChapterID,
+                ]);
+            });
+            this.audio = new Audio(this.currentpath);
+            this.setup();
+            this.loading = false;
+
+            let c = res[2].data.Body.Data["Authority2"];
+            this.Auth = c;
+        });
+    },
+    methods: {
+        del(Chapter) {
+            this.c_pause();
+            let ChapterID = Chapter[3];
+            let UserID = Chapter[2];
+            this.$dialog
+                .confirm(
+                    {
+                        title: "確認",
+                        body: "削除してもよろしいですか？",
+                    },
+                    {
+                        okText: "はい",
+                        cancelText: "キャンセル",
+                        reverse: true,
+                    }
+                )
+                .then(() => {
+                    api.delete(ChapterID, UserID).then((res) => {
+                        this.stamps = this.stamps.filter(
+                            (item) => item[3] != ChapterID
+                        );
+                        this.c_play();
+                    });
+                })
+                .catch(() => {
+                    this.c_play();
+                });
+        },
+        markclick(second) {
+            if (this.audio) {
+                this.audio.currentTime = Math.floor(second);
+            }
+        },
+        c_play() {
+            this.audio.play();
+        },
+        c_pause() {
+            this.audio.pause();
+        },
+        change(v) {
+            this.audio.currentTime = v;
+        },
+        setup() {
+            this.audio.oncanplay = () => {
+                this.max = this.audio.duration;
+            };
+            this.audio.ontimeupdate = () => {
+                this.$refs.timestamp.changeTime(this.audio.currentTime);
+                this.$refs.audiotimebars.timechange(this.audio.currentTime);
+            };
+            this.audio.onpause = () => {
+                this.$refs.controller.stop();
+                this.$refs.audiotimebars.stop();
+            };
+            this.audio.onplay = () => {
+                this.$refs.controller.start();
+            };
+        },
+        next() {
+            if (this.audio.currentTime + 10 > this.audio.duration) {
+                this.audio.currentTime = this.audio.duration;
+            } else {
+                this.audio.currentTime += 10;
+            }
+        },
+        back() {
+            if (this.audio.currentTime < 10) {
+                this.audio.currentTime = 0;
+            } else {
+                this.audio.currentTime -= 10;
+            }
+        },
+        chapteredit(second, Title, cid, userid = NaN) {
+            if (!second) {
+                if (this.audio) {
+                    second = this.audio.currentTime;
+                } else {
+                    second = 0;
+                }
+            }
+            this.second = second;
+            this.title = Title;
+            this.cid = cid;
+            this.userid = userid;
+            this.c_pause();
+            this.$refs.modal.show();
+        },
+        savechapter(second, Title, cid, userid) {
+            this.stamps.filter((item) => {
+                return item[3] !== cid;
+            });
+            if (Number.isNaN(cid)) {
+                api.post(this.currentAudioID, Title, second, userid).then(
+                    (res) => {
+                        this.stamps.push([
+                            second,
+                            Title,
+                            userid,
+                            res.data.Body.ChapterID,
+                        ]);
+                        this.stamps.sort((a, b) => {
+                            return a[0] - b[0];
+                        });
+                    }
+                );
+            } else {
+                api.put(cid, this.currentAudioID, Title, second, userid,this.Auth).then(
+                    (res) => {
+                        this.stamps.map((item) => {
+                            if (item[3] == cid) {
+                                item[0] = second;
+                                item[1] = Title;
+                                item[2] = userid;
+                            }
+                        });
+                    }
+                );
+            }
+            this.$refs.modal.hide();
+            this.c_play();
+        },
+    },
+    computed: {
+        marks() {
+            let marks = [];
+            if (this.stamps) {
+                for (let time of this.stamps) {
+                    marks.push(time[0]);
+                }
+            } else {
+                return null;
+            }
+            return marks;
         },
     },
 };
 </script>
 
-<style lang="scss" scoped>
-.ln-wrapper {
+<style lang="scss">
+.ap-wrapper {
     -webkit-user-select: none;
     -moz-user-select: none;
     -ms-user-select: none;
     user-select: none;
-    .ln-bg {
-        width: 100%;
-        height: 100%;
-        background-color: rgba(30, 30, 30, 0.9);
-        position: fixed;
-        top: 0;
-        left: 0;
-        z-index: 5000;
+    border: 3px solid gray;
+    background-color: #999;
+    .ap-ml {
+        overflow-y: auto;
     }
-    .ln-body {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        width: 98%;
-        max-width: 400px;
-        padding: 15px;
-        background-color: #fff;
-        z-index: 5001;
-        border-radius: 5px;
+    @media screen and (min-width: 1024px) {
+        font-size: 18px;
     }
-    .ln-input {
-        border-bottom: 2px solid #e1e6ea;
-        padding-bottom: 15px;
+    @media screen and (max-width: 1023px) and (min-width: 768px) {
+        font-size: 14px;
     }
-    .ln-ctrl {
-        padding: 15px 0 0;
-        display: flex;
-        justify-content: space-between;
+    @media screen and (max-width: 767px) {
+        font-size: 10px;
     }
-    .ln-b {
-        display: inline-block;
-        height: 40px;
-        min-width: 80px;
-        text-align: center;
-        border-radius: 4px;
-        font-weight: 700;
-        font-size: 16px;
-        padding: 6px 20px;
-        cursor: pointer;
-        &.ln-yes {
-            background-color: white;
-            color: #0096d9;
-            border: #0096d9 2px solid;
-        }
-        &.ln-no {
-            background-color: #0096d9;
-            color: white;
-            border: none;
-        }
+    .ap-title {
+        font-size: 1.8em;
+        text-align: left;
+        margin-top: 10px;
+        margin-left: 10px;
     }
+    .ap-ts-m {
+        z-index: 20;
+    }
+}
+
+.vue-slider-mark,
+.vue-slider-process,
+.vue-slider-dot {
+    z-index: 0 !important;
 }
 </style>
